51697bd134eb4d2ae1106f42dc2280aef8ac893d
unknown <9923103179>
2025-07-30 13:53:39 +0530

    First Commit

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    int num = 0;
+    while(n != 0){
+        int temp = n%10;
+        n/=10;
+        num *= 10;
+        num += temp;
+    }
+    cout<<num<<endl;
+    return 0;
+}
