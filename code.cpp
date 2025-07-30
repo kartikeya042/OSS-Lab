17e437619e283a2888f1c9a5b66176ccd1f590be
unknown <9923103179>
2025-07-30 14:38:38 +0530

    Fixed the code.

--- code.cpp
+++ code.cpp
@@ -4,13 +4,18 @@ int main(){
     int n;
     cout<<"Enter the number:";
     cin>>n;
-    int num = 0;
-    while(n != 0){
-        int temp = n%10;
-        n/=10;
-        num *= 10;
-        num += temp;
+    if(n > 99 && n < 1000){
+        int num = 0;
+        while(n != 0){
+            int temp = n%10;
+            n/=10;
+            num *= 10;
+            num += temp;
+        }
+        cout<<num<<endl;
     }
-    cout<<num<<endl;
+    else{
+        cout<<"Only 3 Digit Numbers Allowed!!";
+    }
     return 0;
 }
